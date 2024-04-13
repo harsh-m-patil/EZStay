da23331f114ea5d1551a000b1e668d1c5b772cdf
harsh-m-patil <humpatil1@gmail.com>
2024-04-13 16:44:58 +0530

    business signup/login done

---
+++ models/business.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const businessSchema = new mongoose.Schema({
+  hotelname: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String
+    
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Business", businessSchema);
---
+++ routes/business.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const business = require("../controllers/business");
+const { verifyToken } = require("../middleware/business");
+
+router.get("/business_signup", business.newBusinesses);
+router.post("/business_signup", business.createBusiness);
+
+// login handler
+router.get("/business_login", business.getBusinesses);
+router.post("/business_login", business.accessBusiness);
+
+router.get('/dashboard',verifyToken,business.businessDashboard)
+// Logout
+router.get("/business_logout", business.logout);
+
+module.exports = router;
