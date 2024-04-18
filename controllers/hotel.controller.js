63002a4dd075931c2fa068592bf30558e301f2d5
vedant-radke <vradke1234@gmail.com>
2024-04-18 20:47:27 +0530

    passing check IN/Out to the hotelInfo page

--- controllers/hotel.controller.js
+++ controllers/hotel.controller.js
@@ -5,43 +5,48 @@ const handleSearchHotel = async (req, res) => {
     let { destination, checkIn, checkOut, guests, clickedHotelName } =
       req.query;
 
-      
     // destination =
     //   destination.charAt(0).toUpperCase() + destination.slice(1).toLowerCase();
 
-      if (!destination) {
-        return res.redirect("index");
-      }
+    if (!destination) {
+      return res.redirect("index");
+    }
 
+    // console.log(`${checkIn} and ${checkOut}`);
 
     const searchedHotels = await Hotel.find({ hotelAddress: destination });
-    
 
     let currDate = new Date();
     let checkInDate = new Date(checkIn);
     let checkOutDate = new Date(checkOut);
 
-    if (checkInDate < currDate) {
-      console.log("Invalid check-in date"); 
-    } else if (checkInDate >= checkOutDate) {
-      console.log("Invalid check-out date");
+    if (checkInDate > currDate) {
+      console.log("Invalid check-in date");
     }
 
-
-    return res.render("searchedHotel", { hotels: searchedHotels });
+    return res.render("searchedHotel", {
+      hotels: searchedHotels,
+      checkIn: checkIn,
+      checkOut: checkOut,
+    });
   } catch (error) {
     console.error("Error searching for hotels:", error);
     res.status(500).json({ message: "Internal server error." });
   }
 };
 
-
-
 async function handleClickedHotel(req, res) {
   let { clickedHotelName, checkIn, checkOut } = req.query;
+
   const clickedHotel = await Hotel.findOne({ hotelName: clickedHotelName });
 
-  return res.render("hotelInfo", { clickedHotel: clickedHotel, checkIn: checkIn, checkOut: checkOut })
+  
+
+  return res.render("hotelInfo", {
+    clickedHotel: clickedHotel,
+    checkIn: checkIn,
+    checkOut: checkOut,
+  });
 }
 
 module.exports = {
--- views/js/searchedHotel.js
+++ views/js/searchedHotel.js
@@ -1,17 +1,27 @@
 const cards = document.querySelectorAll(".card");
 const hotelName = document.querySelectorAll(".hotelName");
+const checkInComing = document.querySelector(".checkInComing");
+const checkOutComing = document.querySelector(".checkOutComing");
 
-// console.log(cards);
+
 
 cards.forEach((card, index) => {
   card.addEventListener("click", () => {
+    const clickedHotelName = hotelName[index].innerHTML;
 
-    
-    // console.log(hotelName[index].innerHTML);
+    const checkInDate = checkInComing.value;
+    const checkOutDate = checkOutComing.value;
 
-    const clickedHotelName = hotelName[index].innerHTML
-    
+    // Construct the URL with parameters
+    var url =
+      "http://localhost:3000/hotelInfo?clickedHotelName=" +
+      encodeURIComponent(clickedHotelName) +
+      "&checkIn=" +
+      encodeURIComponent(checkInDate) +
+      "&checkOut=" +
+      encodeURIComponent(checkOutDate);
 
-    window.location.href = `/hotelInfo?clickedHotelName=${clickedHotelName}`;
+    // Navigate to the hotel info page
+    window.location.href = url;
   });
 });
