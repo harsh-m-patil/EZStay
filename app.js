707e301178434bd5d7a160bb44edcd412e0ca7b0
harsh-m-patil <humpatil1@gmail.com>
2024-04-20 00:43:07 +0530

    fixed business route

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 // const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
-const path = require('path');
-
-
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const path = require("path");
 
 // Configurations
 dotenv.config();
@@ -14,47 +12,38 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 // app.use(session({
-//     secret: 'secretKey', 
+//     secret: 'secretKey',
 //     resave: false,
 //     saveUninitialized: true,
 //     cookie: { secure: false, maxAge: 60000 }
 //   }));
 
-
-
-  app.set('view engine', 'ejs');
-  
+app.set("view engine", "ejs");
 
 // Serve static files
 
 global.staticpath = path.join(__dirname, "views");
 app.use(express.static(staticpath));
-
-
-
+app.use("/business", express.static(staticpath));
 
 // MongoDB connection setup
-const PORT = process.env.PORT || 6000 ;
-mongoose.connect(process.env.url, {
-  
-})
-.then(() => {
-  app.listen(PORT, () => console.log(`server port: ${PORT}`));
-}).catch((error) => console.log(`${error} did not connect`));
-
-
+const PORT = process.env.PORT || 6000;
+mongoose
+  .connect(process.env.url, {})
+  .then(() => {
+    app.listen(PORT, () => console.log(`server port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
 
 // Routes setup
-const hotelroutes = require('./routes/hotel.route');
-app.use('/', hotelroutes);
+const hotelroutes = require("./routes/hotel.route");
+app.use("/", hotelroutes);
 
-const userroutes = require('./routes/user');
-app.use('/', userroutes);
+const userroutes = require("./routes/user");
+app.use("/", userroutes);
 
-const businessroutes = require('./routes/business');
-app.use('/', businessroutes);
+const businessroutes = require("./routes/business");
+app.use("/business", businessroutes);
 
-
-const guestroutes = require('./routes/guest');
-app.use('/', guestroutes);
-
+const guestroutes = require("./routes/guest");
+app.use("/", guestroutes);
--- routes/business.js
+++ routes/business.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const business = require("../controllers/business");
 const { verifyToken } = require("../middleware/business");
 
-router.get("/business_signup", business.newBusinesses);
-router.post("/business_signup", business.createBusiness);
+router.get("/signup", business.newBusinesses);
+router.post("/signup", business.createBusiness);
 
 // login handler
-router.get("/business_login", business.getBusinesses);
-router.post("/business_login", business.accessBusiness);
+router.get("/login", business.getBusinesses);
+router.post("/login", business.accessBusiness);
 
 router.get('/dashboard',verifyToken,business.businessDashboard)
 // Logout
-router.get("/business_logout", business.logout);
+router.get("/logout", business.logout);
 
 module.exports = router;
